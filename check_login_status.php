bf25969d9b0eb39830acd213b44a0b36c68fb5f4
Elvis Dzebo <elvis.dzebo@virginpulse.com>
2024-05-16 02:32:44 +0200

    JSDoc comment for next steps around check_login_status functionality

--- check_login_status.php
+++ check_login_status.php
@@ -1,9 +1,23 @@
 <?php
 session_start();
 
+/**
+ * Let's think a bit about what this part of the code is actually doing.
+ * 1. You define some variable $response
+ * 2. Check if inside of $_SESSION global var is set cerain "korisnik_sesija" value
+ *     ==> If it is, you set $response['isLoggedIn'] to true
+ *     ==> If it's not, you set it to false.
+ * 
+ * How can we do this in a more elegant way? A function maybe?
+ * 
+ * If we can write it as a function, would we still do `echo json_encode()` and than again rely on calling it from .JS
+ *  or we can call this new php function directly in other PHP code?
+ * 
+ * 
+ */
 $response = array();
 
-if(isset($_SESSION['korisnik_sesija'])) {
+if (isset($_SESSION['korisnik_sesija'])) {
     $response['isLoggedIn'] = true;
 } else {
     $response['isLoggedIn'] = false;
